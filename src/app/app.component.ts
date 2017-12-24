2a12ee4e262a833321c345a636d299096ee3e1ed
Juan Carlos <juancarlos.rmr@gmail.com>
2017-12-24 20:36:44 +0100

    funcionalidades de login y singin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AuthProvider } from './providers/auth.provider';
+
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { RoutesPage } from '../pages/routes/routes';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,23 +15,37 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private authProvider: AuthProvider) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Inicio', component: HomePage },
+      { title: 'Rutas', component: RoutesPage }
     ];
 
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.authProvider.Session.subscribe(
+
+        (session) => {
+          console.log("Cambio en la sesion")
+          if (session) {
+            console.log("Usuario logado nos dirigimos a inicio")
+            this.rootPage = HomePage;
+          }
+          else {
+            console.log("Usuario no logado")
+            this.rootPage = LoginPage;
+          }
+        }
+      );
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
@@ -40,5 +57,9 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    this.authProvider.logout();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { RoutesPage } from '../pages/routes/routes';
+import { LoginPage } from '../pages/login/login';
+
+import { AuthProvider } from './providers/auth.provider';
+
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvAQ_kZUsjZToIt2eoe_Mx_rIJtF4HReo",
+  authDomain: "bicli-d6a9c.firebaseapp.com",
+  databaseURL: "https://bicli-d6a9c.firebaseio.com",
+  storageBucket: "bicli-d6a9c.appspot.com",
+  messagingSenderId: '393681130609'
+};
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    RoutesPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    RoutesPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AuthProvider,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
